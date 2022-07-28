dd05bc5c13df5de5bce6c1f408116202cdedd719
Diana Manukyan <20121999_2010@mail.ru>
2022-07-28 21:25:54 +0200

    Fix Layout view

--- webapp/controller/Layout.controller.js
+++ webapp/controller/Layout.controller.js
@@ -1,29 +1,7 @@
 sap.ui.define([
-	'./BaseController',
-	'sap/ui/model/json/JSONModel',
-], function (BaseController, JSONModel, formatter) {
+	"sap/ui/core/mvc/Controller"
+], function (Controller,) {
 	"use strict";
-	return BaseController.extend("sap.ui.demo.bulletinboard.controller.Layout", {
-		onInit: function () {
-			// Model used to manipulate control states. The chosen values make sure,
-			// detail page is busy indication immediately so there is no break in
-			// between the busy indication for loading the view's meta data
-			var oViewModel = new JSONModel({
-					busy: false
-				});
-			this.getRouter().getRoute("Layout").attachPatternMatched(this._onPostMatched, this);
-			this.setModel(oViewModel, "LayoutView");
-		},
-        onNavBack: function () {
-			var oHistory = History.getInstance();
-			var sPreviousHash = oHistory.getPreviousHash();
-
-			if (sPreviousHash !== undefined) {
-				window.history.go(-1);
-			} else {
-				var oRouter = this.getOwnerComponent().getRouter();
-				oRouter.navTo("overview", {}, true);
-			}
-		}
+	return Controller.extend("sap.ui.demo.walkthrough.controller.Layout", {
 	});
 });
