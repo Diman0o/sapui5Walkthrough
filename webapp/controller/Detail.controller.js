283f4603fef44d551c9b1b9e1659800a7e2617d9
Diana Manukyan <20121999_2010@mail.ru>
2022-07-29 09:59:16 +0200

    Finish step 33

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,14 +1,35 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-    "sap/ui/core/routing/History"
-], function (Controller, History) {
+    "sap/ui/core/routing/History",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, History, MessageToast, JSONModel) {
 	"use strict";
 	return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
 		onInit: function () {
+			var oData = {
+				boxes : [
+				   {
+					"key1": "value1",
+				   },
+				   {
+					"key1": "value2",
+				   },
+				   {
+					"key1": "value3",
+				   },
+				   {
+					"key1": "value4",
+				   },
+				]
+			 };
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel, "view");
 			var oRouter = this.getOwnerComponent().getRouter();
 			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
 		},
 		_onObjectMatched: function (oEvent) {
+			this.byId("rating").reset();
 			this.getView().bindElement({
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").gamePath),
 				model: "game",
@@ -28,6 +49,13 @@ sap.ui.define([
 				var oRouter = this.getOwnerComponent().getRouter();
 				oRouter.navTo("overview", {}, true);
 			}
+		},
+
+		onRatingChange: function (oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
 		}
 	});
 });
